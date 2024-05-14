9f389c913907e8c65db344fe1f31af46ba6e0683
Kim Hee Chan <a01049984394k@gmail.com>
2024-05-14 14:45:45 +0900

    publish : 메인페이지
    240514

---
+++ src/component/page/MainPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+//ui
+import Title from "../ui/Title";
+
+function MainPage(props) {
+
+    return (
+        <Title></Title>
+    )   
+
+}
+
+export default MainPage;
---
+++ src/component/ui/Title.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const TitleText = styled.p`
+    font-size: 48px;
+`
+
+function Title(props) {
+
+    return (
+        <TitleText></TitleText>
+    )   
+
+}
+
+export default Title;
